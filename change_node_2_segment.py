e103a1344d6a00632fb6750a41bbbe934cfa3307
Leo <xinming.lai@qq.com>
2019-03-19 20:16:57 +0800

    完成node sequence 到 segment sequence的转换功能

--- change_node_2_segment.py
+++ change_node_2_segment.py
@@ -5,16 +5,30 @@ def main(input_walk, node2segment_dict, output_walk):
     with open(node2segment_dict) as node2segment_file:
         node2segment_line = node2segment_file.readline()
         node2segment = json.loads(node2segment_line)
+    with open(output_walk, 'w+') as f:
+        for segments in get_segments(input_walk, node2segment):
+            f.write('%s\n' % ' 0 '.join(map(str, segments)))
+
+
+def get_segments(input_walk, node2segment):
     for walk_line in input_walk:
         nodes = walk_line.strip().split(' ')
         if len(nodes) < 10:
             continue
         else:
-            trans_node_to_segment(nodes)
+            segments = trans_node_to_segment(nodes, node2segment)
+            yield segments
 
 
-def trans_node_to_segment(array):
-    pass
+def trans_node_to_segment(nodes, node2segment):
+    segments = []
+    last_node = None
+    for node in nodes:
+        if last_node is not None:
+            segment = node2segment[last_node][node]
+            segments.append(segment)
+        last_node = node
+    return segments
 
 
 if __name__ == '__main__':
