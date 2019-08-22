db533c4d5ebcf96d2ad3d227ce4ffa8f8761bec2
Leo Bright <564312023@qq.com>
2019-08-22 08:46:21 +0800

    Bugfix: 修正读取word type文件时的索引问题

--- split_mixture_embeddings.py
+++ split_mixture_embeddings.py
@@ -4,7 +4,7 @@ def split_mixture_embeddings(mixture_embedding_file, word_type_file, output_node
     with open(word_type_file) as f:
         for line in f:
             word_type = line.strip().split()
-            word, type = word_type[0, 1]
+            word, type = word_type[0], word_type[1]
             if type == '0' or type == 0:
                 node_set.add(word)
 
