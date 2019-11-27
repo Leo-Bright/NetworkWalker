f651598b2ef825b8bb92208d38751227a6b20036
Leo Bright <buxingzhexiong@sina.com>
2019-11-27 23:07:17 +0800

    Feature: add extract_segment_from_walk

--- extract_segment_from_walk.py
+++ extract_segment_from_walk.py
@@ -2,9 +2,17 @@ import json
 
 
 def main(input_walk, node2segment_dict, segment_length, output):
+
+    length_dict = {}
+    with open(segment_length) as length_file:
+        for line in length_file:
+            seg, len = line.strip().split(' ')
+            length_dict[seg] = len
+
     with open(node2segment_dict) as node2segment_file:
         node2segment_line = node2segment_file.readline()
         node2segment = json.loads(node2segment_line)
+
     segments_pair = set()
     for _, segments in get_segments(input_walk, node2segment):
         pointer = 0
@@ -18,12 +26,6 @@ def main(input_walk, node2segment_dict, segment_length, output):
                 if t2 not in segments_pair:
                     segments_pair.add(t1)
                 pointer += 1
-
-    length_dict = {}
-    with open(segment_length) as length_file:
-        for line in length_file:
-            seg, len = line.strip().split(' ')
-            length_dict[seg] = len
 
     with open(output, 'w+') as output_file:
         for item in segments_pair:
