d427d2f0c2888a15b0a9c66e47256bdb4ab1eccf
Leo Bright <buxingzhexiong@sina.com>
2019-11-27 23:08:58 +0800

    Feature: add extract_segment_from_walk

--- extract_segment_from_walk.py
+++ extract_segment_from_walk.py
@@ -8,10 +8,12 @@ def main(input_walk, node2segment_dict, segment_length, output):
         for line in length_file:
             seg, len = line.strip().split(' ')
             length_dict[seg] = len
+    print("length_dict load done!")
 
     with open(node2segment_dict) as node2segment_file:
         node2segment_line = node2segment_file.readline()
         node2segment = json.loads(node2segment_line)
+    print("node2segment_dict load done!")
 
     segments_pair = set()
     for _, segments in get_segments(input_walk, node2segment):
@@ -26,6 +28,7 @@ def main(input_walk, node2segment_dict, segment_length, output):
                 if t2 not in segments_pair:
                     segments_pair.add(t1)
                 pointer += 1
+    print("segments_pair load done!")
 
     with open(output, 'w+') as output_file:
         for item in segments_pair:
@@ -66,5 +69,5 @@ if __name__ == '__main__':
     main(input_walk='sanfrancisco/network/sf_random_wn10_wl1280.walks',
          node2segment_dict='sanfrancisco/network/sanfrancisco_nodes2segment.json',
          segment_length='sanfrancisco/network/sanfrancisco.length',
-         output='sanfrancisco/network/sf_random_wn10_wl1280.segments',
+         output='sanfrancisco/network/sanfrancisco_segment.network',
          )
